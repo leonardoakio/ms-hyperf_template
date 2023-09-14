0b999ab2ad5d4477f288d824d2c2ce1096ff164c
Leonardo Akio <akioleonardo@gmail.com>
2023-09-14 10:49:10 -0300

    infra configuration

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -7,9 +7,9 @@ use function Hyperf\Support\env;
 return [
     'default' => [
         'driver' => env('DB_DRIVER', 'mysql'),
-        'host' => env('DB_HOST', 'localhost'),
-        'database' => env('DB_DATABASE', 'cliente-total'),
-        'port' => env('DB_PORT', 3306),
+        'host' => env('DB_HOST', 'hyperf-template_mysql'),
+        'database' => env('DB_DATABASE', 'hyperf-template'),
+        'port' => env('DB_PORT', 3308),
         'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', ''),
         'charset' => env('DB_CHARSET', 'utf8'),
